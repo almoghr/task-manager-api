756f02deb509337a67361369e8419d0229528209
almog ram <almoghr36@gmail.com>
2020-11-17 10:19:15 +0200

    learning crud operations

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,12 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectId, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager-app'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if(error){
         return console.log('unable to connect to mongodb database')
     } 
-    console.log('mongoDB connected successfully')
-})
+    const db = client.db(databaseName)
+});
