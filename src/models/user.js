a5e77da4eeb685e43d501d10358beeb578144301
almog ram <almoghr36@gmail.com>
2020-11-20 23:27:56 +0200

    added hashing passwords ability through bcryptjs

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,7 @@
 const mongoose = require ('mongoose')
 const validator = require ('validator')
-
-
-const User = mongoose.model('User', {
+const bcryptjs = require('bcryptjs')
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -47,5 +46,15 @@ const User = mongoose.model('User', {
     },      
 });
 
+userSchema.pre('save', async function (next) {
+    const user = this
+    if(user.isModified('password')){
+        user.password = await bcryptjs.hash(user.password, 8)
+    }
+    next()
+})
 
-module.exports = User
+const User = mongoose.model('User', userSchema) 
+
+
+module.exports = User   
