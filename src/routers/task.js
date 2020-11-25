4969a192a57c993bc97918fcd0c1b49a73cb0517
almog ram <almoghr36@gmail.com>
2020-11-25 12:29:58 +0200

    sorting, pagination and skip is set on the back end services, started working with multer

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,9 +17,28 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+    if (req.query.completed){
+        match.completed = req.query.completed === "true"
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(":")
+        console.log(parts)
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
     try{
-        const tasks = await Task.find({ owner: req.user._id })
-        res.send(tasks)
+        await req.user.populate({ 
+            path: 'tasks',
+            match,
+            options:{
+                limit: +req.query.limit || 10,
+                skip: +req.query.skip,
+                sort
+            } 
+        }).execPopulate()
+        res.send(req.user.tasks)
     } catch(err){
         res.status(500).send(err)
     }
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const multer = require ('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -76,6 +78,15 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+
+const upload = multer({
+    dest: 'avatars'
+})
+
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+        res.send()
+})
 
 
 module.exports = router
